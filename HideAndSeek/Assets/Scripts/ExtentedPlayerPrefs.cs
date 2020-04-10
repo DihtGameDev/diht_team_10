08ea79c145fce52df5db92e415022c4d53ffa2e7
Egor <jux7ent@yandex.ru>
2020-04-10 22:14:50 +0300

    added ExtentedPlayerPrefs

---
+++ HideAndSeek/Assets/Scripts/ExtentedPlayerPrefs.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public static class ExtentedPlayerPrefs {
+    public static void SetObject<T>(string key, T obj) {
+        PlayerPrefs.SetString(key, JsonUtility.ToJson(obj));
+    }
+
+    public static T GetObject<T>(string key) where T : class {
+        string jsonString = PlayerPrefs.GetString(key, "");
+        if (jsonString == "") {
+            return null;
+        } else {
+            return JsonUtility.FromJson<T>(jsonString);
+        }
+    }
+}
--- HideAndSeek/Assets/Scripts/PrefsStorage.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface PrefsStorage {
-    T Deserealize<T>(string key) where T : class;
-    void Serialize<T>(string key, T obj);
-}
