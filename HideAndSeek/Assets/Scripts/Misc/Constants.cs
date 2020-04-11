6261b5ee091defca6c3897c0cca0e84cab25f2e6
Egor <jux7ent@yandex.ru>
2020-04-11 22:43:59 +0300

    added settings and constants files

---
+++ HideAndSeek/Assets/Scripts/Misc/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Constants {
+    public static class PlayerPrefs {
+        public static string SETTINGS = "settings";
+    }
+}
