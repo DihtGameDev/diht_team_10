30f6f2b60e0c8b6537b00dd7ed95325d29d60e5f
Egor <jux7ent@yandex.ru>
2020-04-11 20:03:17 +0300

    added one player scene

---
+++ HideAndSeek/Assets/Scripts/CameraController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [SerializeField]
+    private GameObject _chasingObj;
+
+    private Vector3 _deltaChasingObjPos;
+    private float _cameraChasingLerp = 0.125f;
+
+    private Renderer fadedRenderer = null;
+    private GameObject fadedGameObject = null; // optimization
+
+    private void Start() {
+        if (_chasingObj == null) {
+            SetChasingObject(GameObject.FindGameObjectsWithTag("Player")[0]);
+        } else {
+            _deltaChasingObjPos = transform.position - _chasingObj.transform.position;
+        }
+
+        StartCoroutine("FadeOverlappingObjects");
+    }
+
+    private void Update() {
+        if (_chasingObj != null) {
+            transform.position = Vector3.Lerp(transform.position, _chasingObj.transform.position + _deltaChasingObjPos, _cameraChasingLerp);
+        }
+    }
+
+    private void SetChasingObject(GameObject chasingObj) {
+        _chasingObj = chasingObj;
+        _deltaChasingObjPos = transform.position - _chasingObj.transform.position;
+    }
+    
+    private IEnumerator FadeOverlappingObjects() {
+        int layerMask = LayerMask.GetMask("Obstacle"); // = 1 << 9;
+
+        Vector3 dir = _chasingObj.transform.position - transform.position;
+        dir = dir / dir.magnitude; // normalize
+
+        while (true) {
+            Renderer r = _CheckOverlapping(dir, layerMask);
+
+            if (r != fadedRenderer) {
+                if (fadedRenderer != null) {
+                    fadedRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.On;
+                }
+                
+                if (r != null) {
+                    r.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+                }
+
+                fadedRenderer = r;
+            }
+
+            yield return new WaitForSeconds(.2f);
+        }
+    }
+
+    private Renderer _CheckOverlapping(Vector3 dir, int layerMask) {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, dir, out hit, Mathf.Infinity, layerMask)) {
+            Debug.DrawRay(transform.position, dir * hit.distance, Color.yellow);
+            if (hit.collider.gameObject == fadedGameObject) {
+                return fadedRenderer;
+            } else {
+                fadedGameObject = hit.collider.gameObject;
+                return fadedGameObject.GetComponent<Renderer>();
+            }
+        } else {
+            fadedGameObject = null;
+            Debug.DrawRay(transform.position, dir * 1000, Color.white);
+            return null;
+        }
+    }
+}
---
+++ HideAndSeek/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+    private Rigidbody _rb;
+
+    [SerializeField]
+    private float _lerpAngleCoeff = 0.08f;
+
+    public float moveSpeed = 0.03f;
+
+    public Joystick _moveJoystick;
+
+    private void Start() {
+        _rb = GetComponent<Rigidbody>();
+        _moveJoystick = GameObject.Find("Canvas").GetComponentInChildren<Joystick>();
+
+        if (_moveJoystick == null) {
+            print("Needs FixedJoystick on this scene");
+            throw new UnassignedReferenceException();
+        }
+    }
+
+    private void Update() {
+        _Move();
+        _rb.velocity = Vector3.zero;
+        _rb.angularVelocity = Vector3.zero;
+    }
+
+    private void _Move() {
+        Vector3 pos = transform.position;
+        pos.x += _moveJoystick.Direction.x * 0.03f;
+        pos.z += _moveJoystick.Direction.y * 0.03f;
+        transform.position = pos;
+        if (_moveJoystick.Direction != Vector2.zero) {
+            float angle = Mathf.Rad2Deg * Mathf.Atan(_moveJoystick.Direction.x / _moveJoystick.Direction.y);
+            if (_moveJoystick.Direction.y < 0) {
+                angle -= 180f;
+            }
+
+            transform.eulerAngles = new Vector3(0,
+                                                Mathf.LerpAngle(transform.eulerAngles.y, angle + 180f, 0.08f),
+                                                0);
+        }
+    }
+}
